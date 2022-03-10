8337dce86d7eab9926a8055767017e232fff09bb
Dawid Lubowiecki <lubowiecki@gmail.com>
2022-03-10 15:02:07 +0100

    file

--- src/mock-proxy/modules/user/user.ts
+++ src/mock-proxy/modules/user/user.ts
@@ -1,19 +1,30 @@
 import http from 'http';
 
+import { createReadStream, readFileSync, statSync } from 'fs-extra';
 import { HttpAdapters, HttpRequestWithBody } from '@lubowiecki/node-utility';
 
 import { userDtoOne } from './examples/user-dto-one';
+
+const modifyResponse = require('node-http-proxy-json');
 
 export class User {
 	processReq(proxyReq: http.ClientRequest, req: HttpRequestWithBody, res: http.ServerResponse): void {}
 
 	processRes(proxyRes: http.IncomingMessage, req: http.IncomingMessage, res: http.ServerResponse): void {
-		if (req.url?.match(`^/user/id/${userDtoOne.id}`) && req.method === 'GET') {
+		if (req.url?.match(`^/user/${userDtoOne.id}`) && req.method === 'GET') {
 			HttpAdapters.replaceResponseBody(userDtoOne, proxyRes, res);
 		}
 
-		if (req.url?.match(`^/user/id/${userDtoOne.id}`) && req.method === 'PUT') {
+		if (req.url?.match(`^/user/${userDtoOne.id}`) && req.method === 'PUT') {
 			HttpAdapters.replaceResponseBody(userDtoOne, proxyRes, res);
+		}
+
+		if (req.url?.match(`^/user/cv/${userDtoOne.id}`) && req.method === 'GET') {
+			const file = readFileSync('src/static/files/test.txt');
+
+			proxyRes.headers['content-type'] = 'text/plain';
+
+			modifyResponse(res, proxyRes, (body: any) => file.toString());
 		}
 	}
 }

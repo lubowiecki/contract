53f6813649f10f2a3b47c7fa11982f20407120a6
Dawid Lubowiecki <dawid.lubowiecki@opi.org.pl>
2023-03-23 15:25:02 +0100

    dates

--- src/mock/dtos/models/iso-date-with-time-dto.ts
+++ src/mock/dtos/models/iso-date-with-time-dto.ts
@@ -2,6 +2,6 @@
 /* eslint-disable */
 
 /**
- * local: 2020-09-15T09:01:05, UTC: 2020-09-15T09:01:05Z
+ * local: 2020-09-15T09:01:05, UTC: 2020-09-15T09:01:05Z, Pattern: ^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z?$
  */
 export type IsoDateWithTimeDto = string;
